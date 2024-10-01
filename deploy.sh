a46fdaea67fd390ce9352c7871c750d7843eb4c1
Jewels2001 <juliewq25@gmail.com>
2024-09-30 23:06:56 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ cd ..
 git add -A
 git commit -m "deploy `$current_time`"
 
-# git push -f git@github.com:jewels2001/jewels2001.github.io/board-games.git main:gh-pages
+git push -f git@github.com:jewels2001/jewels2001.github.io/board-games.git main:gh-pages
 git subtree pull --prefix board-game-blog/dist origin main:gh-pages --squash
